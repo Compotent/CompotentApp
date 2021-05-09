9630e9f488f628b148fb8bbb0b871f09f8257d25
MarinaNem <marina.laimik@gmail.com>
2021-05-09 15:50:54 +0500

    add icon and change navbar

--- WebApp/ClientApp/src/components/NavMenu.js
+++ WebApp/ClientApp/src/components/NavMenu.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { LoginMenu } from './api-authorization/LoginMenu';
 import './NavMenu.css';
+
+const navbar = {backgroundColor: "#0c0e38", color: "white"};
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
@@ -21,25 +23,16 @@ export class NavMenu extends Component {
       collapsed: !this.state.collapsed
     });
   }
-
+  
   render () {
     return (
       <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
+        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-0.5 text-white" style={navbar}>
           <Container>
-            <NavbarBrand tag={Link} to="/">WebApp</NavbarBrand>
+            <NavbarBrand tag={Link} to="/">CompotentApp - Редактор изображения</NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-                </NavItem>
                 <LoginMenu>
                 </LoginMenu>
               </ul>
